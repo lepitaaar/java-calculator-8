2fc964ce73351716a0db1477f5b714a7173a10a4
lepitaaar <leekus1005@gmail.com>
2025-10-16 22:05:24 +0900

    feature: separate ',' and sum all number

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = readLine();
+
+        String[] nums = input.split(",");
+
+        int sum = 0;
+
+        for (String num : nums) {
+            int readNum = Integer.parseInt(num);
+            sum += readNum;
+        }
+
+        System.out.println("결과 : " + sum);
+
     }
 }
