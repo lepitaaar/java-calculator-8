dae6f29ab9acbcb4eafc250dc3329682cf98ee6b
lepitaaar <leekus1005@gmail.com>
2025-10-20 18:53:37 +0900

    refactor: Calculator의 파싱로직 이관
    Calculator가 가지던 과한 책임을 분리하여
    Calculator는 오직 계산. 파싱과 숫자변환은 Numbers가 가지게 변경

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -1,4 +1,36 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Numbers {
+    private final String[] DEFAULT_DELIMITERS = new String[]{",", ":"};
+    private final List<String> delimiters = new ArrayList<>(Arrays.asList(DEFAULT_DELIMITERS));
+    private final List<Integer> numbers;
+
+    public Numbers(String input) {
+        if (input.startsWith("//")) {
+            int endIndex = input.indexOf("\\n");
+            addDelimiter(input.substring(2, endIndex));
+            input = input.substring(endIndex + 2);
+        }
+
+        String[] nums = input.split(getDelimiter());
+        this.numbers = Arrays.stream(nums)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    public void addDelimiter(String delimiter) {
+        delimiters.add(delimiter);
+    }
+
+    public String getDelimiter() {
+        return String.join("|", delimiters);
+    }
+
+    public List<Integer> toList() {
+        return numbers;
+    }
 }
