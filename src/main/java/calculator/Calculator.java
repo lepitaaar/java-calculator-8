f2d9085c6cec1374f9b5dd2f4788814e7f6f6f52
lepitaaar <leekus1005@gmail.com>
2025-10-20 18:54:43 +0900

    refactor: Calculator의 계산 로직 리팩터링
    Calculator는 오직 계산만 하게 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,34 +1,7 @@
 package calculator;
 
-import java.util.Arrays;
-
 public class Calculator {
-    private final String input;
-    private int sum;
-    private final Delimiter delimiter;
-
-    public Calculator(String input) {
-        this.input = input;
-        this.sum = 0;
-        this.delimiter = new Delimiter(input);
-    }
-
-    public int calculate() {
-        boolean isCustom = delimiter.isUsingCustomDelimiter();
-
-        if (isCustom) {
-            String customDelimiter = delimiter.getCustomDelimiter();
-            delimiter.addCustomDelimiter(customDelimiter);
-        }
-
-        String[] nums = input.split(delimiter.getDelimiter());
-
-        return Arrays.stream(nums)
-                .map(Integer::parseInt)
-                .mapToInt(Integer::intValue)
-                .peek(n -> {
-                    if (n < 0) throw new IllegalArgumentException();
-                })
-                .sum();
+    public int calculate(Numbers numbers) {
+        return numbers.toList().stream().mapToInt(i -> i).sum();
     }
 }
