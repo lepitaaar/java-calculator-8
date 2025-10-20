4bf78fedf76c4fd3a88f39932f9f5be3c81dbc3f
lepitaaar <leekus1005@gmail.com>
2025-10-20 18:42:30 +0900

    refactor: 메인이 가지는 계산 로직 Calculator로 이동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,31 +1,15 @@
 package calculator;
 
-import java.util.Arrays;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         String input = readLine();
         Calculator calculator = new Calculator(input);
-        boolean isCustom = Delimiter.isUsingCustomDelimiter(input);
 
-        if (isCustom) {
-            String delimiter = Delimiter.getCustomDelimiter(input);
-            Delimiter.addCustomDelimiter(delimiter);
-        }
+        int result = calculator.calculate();
 
-        String[] nums = input.split(Delimiter.getDelimiter());
-
-        int sum = Arrays.stream(nums)
-                .map(Integer::parseInt)
-                .mapToInt(Integer::intValue)
-                .peek(n -> {
-                    if (n < 0) throw new IllegalArgumentException();
-                })
-                .sum();
-
-        System.out.println("결과 : " + sum);
+        System.out.println("결과 : " + result);
 
     }
 }
--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -1,25 +1,32 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Delimiter {
-    static List<String> delimiters = Arrays.asList(",", ":");
+    private List<String> delimiters = new ArrayList<>(Arrays.asList(",", ":"));
+    private String input;
 
-    public static boolean isUsingCustomDelimiter(String input) {
+    public Delimiter(String input) {
+        this.input = input;
+    }
+
+    public boolean isUsingCustomDelimiter() {
         return input.startsWith("//");
     }
 
-    public static String getCustomDelimiter(String input) {
+    public String getCustomDelimiter() {
         int endIndex = input.indexOf("\\n");
+        input = input.substring(endIndex + 2);
         return input.substring(2, endIndex);
     }
 
-    public static void addCustomDelimiter(String delimiter) {
+    public void addCustomDelimiter(String delimiter) {
         delimiters.add(delimiter);
     }
 
-    public static String getDelimiter() {
+    public String getDelimiter() {
         return String.join("|", delimiters);
     }
 }
