6e88ce1747ff1414b86436291dc619612a22e5a6
lepitaaar <leekus1005@gmail.com>
2025-10-20 18:54:58 +0900

    refactor: 계산 로직 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,9 +5,9 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         String input = readLine();
-        Calculator calculator = new Calculator(input);
-
-        int result = calculator.calculate();
+        Numbers numbers = new Numbers(input);
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(numbers);
 
         System.out.println("결과 : " + result);
 
